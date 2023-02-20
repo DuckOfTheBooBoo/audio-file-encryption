d781d018a0086cc2cae60d0104aea0da432e8f34
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-02-20 14:00:03 +0700

    Change from Crypto.Cipher to cryptography.fernet

--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,33 @@
 from os.path import join, isfile, isdir
 from os import remove, rename, environ, listdir, mkdir
 from io import BytesIO
+from cryptography.fernet import Fernet
+import uuid
 
 INPUT_DIR = "input"
 OUTPUT_DIR = "output"
 AUDIO_FILE_EX = (".mp3", ".wav", ".ogg")
 
 # Get filelist
-files = [file for file in listdir(INPUT_DIR) if isfile(file) and file.endswith(AUDIO_FILE_EX)]
+files = [file for file in listdir(INPUT_DIR) if file.endswith(AUDIO_FILE_EX)]
 
-# Get encryption key from environment variable
-en_key = environ.get("en_key")
+# Get encryption key from environment variable and encode it
+env_key = environ.get("en_key").encode("utf-8")
+key = Fernet(env_key)
 
 def encrypt():
-    pass    
-
+    for file in files:
+        
+        audiofile = join(INPUT_DIR, file)
+        with open(audiofile, 'rb') as infile:
+            data = infile.read()
+            
+        encrypted_data = key.encrypt(data)
+        filename = str(uuid.uuid4())
+        
+        with open(join(OUTPUT_DIR, filename), 'wb') as outfile:
+            print(f"out -> {join(OUTPUT_DIR, filename)}")
+            outfile.write(encrypted_data)
+            
 if __name__ == "__main__":
     encrypt()
