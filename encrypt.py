388fca7bb3e092f5b3366ada30fcef0846513f48
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-02-20 10:55:46 +0700

    Added file list with audio file only

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,19 @@
 from os.path import join, isfile
-from os import remove, rename, environ
+from os import remove, rename, environ, listdir
+from io import BytesIO
 
 INPUT_DIR = "input"
 OUTPUT_DIR = "output"
+AUDIO_FILE_EX = (".mp3", ".wav", ".ogg")
 
 # Get filelist
+files = [file for file in listdir(INPUT_DIR) if isfile(file) and file.endswith(AUDIO_FILE_EX)]
 
 # Get encryption key from environment variable
 en_key = environ.get("en_key")
 
 def encrypt():
-    pass
+    pass    
 
 if __name__ == "__main__":
     encrypt()
