a125c0952af147780d1c8e9024e2819dec1232a6
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-02-20 09:57:30 +0700

    Created encrypt and decrypt function

---
+++ decrypt.py
@@ -0,0 +1,8 @@
+from os.path import join, isfile
+from os import environ
+
+def decrypt():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ encrypt.py
@@ -0,0 +1,16 @@
+from os.path import join, isfile
+from os import remove, rename, environ
+
+INPUT_DIR = "input"
+OUTPUT_DIR = "output"
+
+# Get filelist
+
+# Get encryption key from environment variable
+en_key = environ.get("en_key")
+
+def encrypt():
+    pass
+
+if __name__ == "__main__":
+    encrypt()
