893439a39d6424daa4e4c040eda0dedd7e193d9c
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-02-21 19:53:27 +0700

    Return audio data in bytes and audio metadata in id3

--- decrypt.py
+++ decrypt.py
@@ -1,10 +1,14 @@
 from os.path import join, isfile
 from os import environ, listdir
 from io import BytesIO
+from mutagen.id3 import ID3, APIC
 from cryptography.fernet import Fernet, InvalidToken
 
 INPUT_DIR = "input"
 OUTPUT_DIR = "output"
+AUDIO_COVER_ART = "APIC:cover"
+AUDIO_TITLE = "TIT2"
+AUDIO_ARTIST = "TPE1"
 
 # Get filelist
 files = [file for file in listdir(OUTPUT_DIR) if file.endswith(".enc")]
@@ -27,12 +31,17 @@ def decrypt():
                 encrypted_data = infile.read()
                 
                 decrypted_data = key.decrypt(encrypted_data)
+
+                data = BytesIO(decrypted_data)
+                
+                audio = ID3(data)
             
-            return decrypted_data
-        
+            return (data, audio)
+                
         except InvalidToken:
             print("The key does not match with the key that used when encrypting the file, thus cannot be used.")
-        
+
+            return None
 
 if __name__ == "__main__":
     decrypt()
