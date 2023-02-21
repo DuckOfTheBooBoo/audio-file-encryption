e9d200b3acdb2e87f5f37d31fdf68593ce3f4ab7
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-02-21 21:00:09 +0700

    recommit

--- tests/unit/test_decrypt.py
+++ tests/unit/test_decrypt.py
@@ -1,4 +1,8 @@
+# TODO: Fix importing module
+
 from src.decrypt import decrypt
 
+
+# TODO: put encrypted audio file in the first test case
 def test_title_artist():
     assert decrypt() == "OMG - NewJeans"
