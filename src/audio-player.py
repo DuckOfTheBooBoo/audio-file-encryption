f1836037733c4c7ec2d2419aacd5d10e87788e32
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-02-21 21:03:54 +0700

    fix src untracked

---
+++ src/audio-player.py
@@ -0,0 +1,20 @@
+from rich.progress import Progress, BarColumn, TimeRemainingColumn
+from rich.traceback import install
+from playsound import playsound
+
+install()
+
+def play_audio(file_path):
+    
+    length = playsound(file_path, True) // 1000
+    
+    with Progress("[progress.description]{task.description}", BarColumn(), "{task.completed}/{task.total}", TimeRemainingColumn()) as progress:
+        task = progress.add_task("Playing audio...", total=length)
+        playsound(file_path, False)
+        for i in range(length):
+            progress.update(task, advance=1)
+        playsound.stop()
+
+if __name__ == "__main__":
+    file_path = r"input\NewJeans - OMG.wav"
+    play_audio(file_path)
